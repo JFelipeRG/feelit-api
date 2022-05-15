47a260f696215a164a8ed8886f68a59ce5f704f4
Felipe Ramirez <juanfel2024@gmail.com>
2022-05-15 14:11:01 +0200

    configuracion antes de sequelize

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const routes = require('./rutas/routes')
+const cors = require('cors')
 const port = 3002
 const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 const server = app.listen(port, (err) => {
     if(err) return console.log(`Error: ${err}`)
--- rutas/routes.js
+++ rutas/routes.js
@@ -1,14 +1,25 @@
 const pool = require('../data/config')
 
 const router = app => {
-    app.get('/', (request, response) => {
-        response.send({
-            message: 'Node.js and Express REST API'
-        });
+
+    app.get('/api', (request, response) => {
+        response.send('<h1>Welcome to the Feel It API</h1');
     });
 
-    app.get('/users', (request, response) => {
-        pool.query('SELECT * FROM usuarios', (error, result) => {
+    app.post('/api/login', (request, response) => {
+        const { body } = request
+        const { nick, passw } = body
+        pool.query('SELECT * FROM `usuarios` WHERE nick = ? && passw= ?',[nick, passw ], (error, result) => {
+            if (error) throw error;
+     
+            response.send(result);
+        });
+    })
+
+    app.post('/api/register', (request, response) => {
+        const { body } = request
+        const { name, nick, passw, img } = body
+        pool.query('INSERT INTO `usuarios` (`id`, `name`, `nick`, `passw`, `profile_img`, `fecha_creacion`) VALUES (\'\',?,?,?,?,current_timestamp())',[name, nick, passw, img ], (error, result) => {
             if (error) throw error;
      
             response.send(result);
