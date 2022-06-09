0a8d9437f3dfadf7f07fcbd76b941ffff771ae12
Felipe Ramirez <juanfel2024@gmail.com>
2022-06-09 13:58:11 +0200

    prueba

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-require('./rutas/routes')(app)
+app.use('/', (req, res) => {
+    res.json({ message: 'Bienvenido a la API de Feel It' })
+})
 
 const server = app.listen(port, (err) => {
     if(err) return console.log(`Error: ${err}`)
